0047958e103e493d5449ed4395fb050e0619feae
dao0dao <58562964+dao0dao@users.noreply.github.com>
2020-03-06 20:36:38 +0100

    Add files via upload

--- js/lazyLoading.js
+++ js/lazyLoading.js
@@ -5,6 +5,8 @@ let $personelPhoto; // Zdjęcie kadry
 let $personelText; // Tło kadry
 let $pirce; // Sekcja cennik
 let $priceImage; // Tło cennika
+let $contact; // Kontakt
+let $contactMap; // Mapa googla
 
 const $main = () => {
     $prepairDomElemnts();
@@ -22,6 +24,9 @@ const $prepairDomElemnts = () => {
 
     $pirce = document.querySelector('#Price');
     $priceImage = document.querySelector('#Price .priceImage');
+
+    $contact = document.querySelector('#Contact');
+    $contactMap = document.querySelector('#Contact iframe');
 };
 //Events
 const $prepairDomEvents = () => {
@@ -52,6 +57,9 @@ const showGalery = function () {
         $priceImage.style.backgroundImage = `url(${$priceImage.dataset.lazy_small_price_background})`
     } else if (window.scrollY + window.innerHeight > $pirce.offsetTop && window.innerWidth >= 992) {
         $priceImage.style.backgroundImage = `url(${$priceImage.dataset.lazy_big_price_background})`
+    };
+    if (window.scrollY + window.innerHeight > $contact.offsetTop) {
+        $contactMap.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1199.5624658016432!2d18.682028658276362!3d53.0360919949788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTPCsDAyJzA5LjkiTiAxOMKwNDAnNTkuMiJF!5e0!3m2!1spl!2spl!4v1582222044744!5m2!1spl!2spl"
     };
 }
 

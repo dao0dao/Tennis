cab2cf258cb39093f98c309a1abb4ce45c9b6f72
dao0dao <58562964+dao0dao@users.noreply.github.com>
2020-04-07 13:41:32 +0200

    Add files via upload
    Fixed scrollspy

--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -9,24 +9,35 @@ document.addEventListener('DOMContentLoaded', () => {
     $tabletMenu = document.querySelectorAll('#Tablet li');
 
     const scrollSpy = () => {
+        const windowWidth = window.innerWidth
         //mobile
-        for (i = 0; i < $mobileMenu.length; i++) {
-            if (window.scrollY + 100 >= $section[i].offsetTop && window.scrollY + 100 < $section[i].offsetTop + $section[i].clientHeight) {
-                $mobileMenu[i].classList.add('Spyscroll');
-            } else {
-                $mobileMenu[i].classList.remove('Spyscroll');
-            };
+        if (windowWidth < 576) {
+            for (i = 0; i < $mobileMenu.length; i++) {
+                if (window.scrollY + 100 >= $section[i].offsetTop && window.scrollY + 100 < $section[i].offsetTop + $section[i].clientHeight) {
+                    $mobileMenu[i].classList.add('Spyscroll');
+                } else {
+                    $mobileMenu[i].classList.remove('Spyscroll');
+                };
 
-            //tablet+
-        };
-        for (i = 0; i < $tabletMenu.length; i++) {
-            if (window.scrollY + 10 >= $section[i].offsetTop && window.scrollY < $section[i].offsetTop + $section[i].clientHeight - 150) {
-                $tabletMenu[i].classList.add('Spyscroll');
-            } else {
-                $tabletMenu[i].classList.remove('Spyscroll');
+                //tablet+
             };
-
-        };
+        } else if (windowWidth < 768) {
+            for (i = 0; i < $tabletMenu.length; i++) {
+                if (window.scrollY + 50 >= $section[i].offsetTop && window.scrollY + 50 < $section[i].offsetTop + $section[i].clientHeight) {
+                    $tabletMenu[i].classList.add('Spyscroll');
+                } else {
+                    $tabletMenu[i].classList.remove('Spyscroll');
+                };
+            };
+        } else {
+            for (i = 0; i < $tabletMenu.length; i++) {
+                if (window.scrollY + 230 >= $section[i].offsetTop && window.scrollY + 230 < $section[i].offsetTop + $section[i].clientHeight) {
+                    $tabletMenu[i].classList.add('Spyscroll');
+                } else {
+                    $tabletMenu[i].classList.remove('Spyscroll');
+                };
+            };
+        }
     };
 
     window.addEventListener('scroll', () => {
